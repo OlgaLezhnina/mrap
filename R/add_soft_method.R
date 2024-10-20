62ee26e18f49074d0f8621ccff2e7e73148faa6d
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2024-10-20 11:18:22 +0200

    fix labels

--- R/add_soft_method.R
+++ R/add_soft_method.R
@@ -11,13 +11,12 @@ add_soft_method <- function(pack, fun) {
   version_R <-
     paste0(session$R.version$major, ".", session$R.version$minor)
   version_pack <- as.character(utils::packageVersion(pack))
-  method_name <- paste0(pack, "::", fun)
   software <- dt$software(label = "R",
                           versioninfo = version_R)
-  soft_library <- dt$software_library(label = method_name,
+  soft_library <- dt$software_library(label = pack,
                                       versioninfo = version_pack,
                                       part_of = software)
-  soft_method <- dt$software_method(label = method_name,
+  soft_method <- dt$software_method(label = fun,
                                     uses_software = soft_library)
   return(soft_method)
 }
--- tests/testthat/test-add_soft_method.R
+++ tests/testthat/test-add_soft_method.R
@@ -1,5 +1,5 @@
 test_that("add_soft_method creates an instance with the correct label", {
   dt <- dtreg::load_datatype("https://doi.org/21.T11969/ff5e3f857788d20dd1aa")
   soft_mehod_inst <- add_soft_method("stats", "aov")
-  expect_equal(soft_mehod_inst$label, "stats::aov")
+  expect_equal(soft_mehod_inst$label, "aov")
 })
