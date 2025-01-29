7aa72ab1d0b699b321c08109f8dffe7090d57922
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2025-01-29 20:07:58 +0100

    new add_soft_method with tests

--- R/add_soft_method.R
+++ R/add_soft_method.R
@@ -1,22 +1,23 @@
 #' Add a software method instance
 #'
-#' @param pack The package name as a string
-#' @param fun The function name as a string
+#' @param pack_fun A string in format "package::function"
+#' @param dt A datatype loaded with the dtreg package
 #'
 #' @return soft_method A soft_method schema instance
 #' @noRd
 #'
-add_soft_method <- function(pack, fun) {
+add_soft_method <- function(dt, pack_fun) {
+  pack <- strsplit(pack_fun, split = "::")[[1]][[1]]
   session <- utils::sessionInfo()
-  version_R <-
+  version_r <-
     paste0(session$R.version$major, ".", session$R.version$minor)
   version_pack <- as.character(utils::packageVersion(pack))
   software <- dt$software(label = "R",
-                          versioninfo = version_R)
+                          versioninfo = version_r)
   soft_library <- dt$software_library(label = pack,
                                       versioninfo = version_pack,
                                       part_of = software)
-  soft_method <- dt$software_method(label = fun,
-                                    uses_software = soft_library)
+  soft_method <- dt$software_method(label = pack_fun,
+                                    part_of = soft_library)
   return(soft_method)
 }
--- tests/testthat/test-add_soft_method.R
+++ tests/testthat/test-add_soft_method.R
@@ -1,5 +1,17 @@
-test_that("add_soft_method creates an instance with the correct label", {
-  dt <- dtreg::load_datatype("https://doi.org/21.T11969/ff5e3f857788d20dd1aa")
-  soft_mehod_inst <- add_soft_method("stats", "aov")
-  expect_equal(soft_mehod_inst$label, "aov")
+test_that("add_soft_method creates an instance of software_method", {
+  dt <- dtreg::load_datatype("https://doi.org/21.T11969/b9335ce2c99ed87735a6")
+  soft_mehod_inst <- add_soft_method(dt, "stats::aov")
+  expect_equal(dt$software_method()$dt_name, soft_mehod_inst$dt_name)
+})
+
+test_that("add_soft_method writes the method label correctly", {
+  dt <- dtreg::load_datatype("https://doi.org/21.T11969/b9335ce2c99ed87735a6")
+  soft_mehod_inst <- add_soft_method(dt, "stats::aov")
+  expect_equal(soft_mehod_inst$label, "stats::aov")
+})
+
+test_that("add_soft_method writes the software library label correctly", {
+  dt <- dtreg::load_datatype("https://doi.org/21.T11969/b9335ce2c99ed87735a6")
+  soft_mehod_inst <- add_soft_method(dt, "stats::aov")
+  expect_equal(soft_mehod_inst$part_of$label, "stats")
 })
