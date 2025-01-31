406036be7fdbebe5b3245faf3a73f426d20e5351
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2025-01-31 13:03:09 +0100

    change method name

--- R/add_soft_method.R
+++ R/add_soft_method.R
@@ -1,23 +1,25 @@
 #' Add a software method instance
 #'
-#' @param pack_fun A string in format "package::function"
+#' @param pack_fun A string in format "package_function"
 #' @param dt A datatype loaded with the dtreg package
 #'
 #' @return soft_method A soft_method schema instance
 #' @noRd
 #'
 add_soft_method <- function(dt, pack_fun) {
-  pack <- strsplit(pack_fun, split = "::")[[1]][[1]]
+  parts <- strsplit(pack_fun, split = "_")[[1]]
+  lib <- parts[[1]]
+  fun <- parts[[2]]
   session <- utils::sessionInfo()
   version_r <-
     paste0(session$R.version$major, ".", session$R.version$minor)
-  version_pack <- as.character(utils::packageVersion(pack))
+  version_lib <- as.character(utils::packageVersion(lib))
   software <- dt$software(label = "R",
                           versioninfo = version_r)
-  soft_library <- dt$software_library(label = pack,
-                                      versioninfo = version_pack,
+  soft_library <- dt$software_library(label = lib,
+                                      versioninfo = version_lib,
                                       part_of = software)
-  soft_method <- dt$software_method(label = pack_fun,
+  soft_method <- dt$software_method(label = fun,
                                     part_of = soft_library)
   return(soft_method)
 }
