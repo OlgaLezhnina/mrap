d686ed3f73c6fa8910a0d23bc507d464dbfd1446
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2025-02-08 11:39:53 +0100

    add stats_aov function

---
+++ R/stats_aov.R
@@ -0,0 +1,68 @@
+#' Get the name of the data from an ANOVA object
+#' @param aov_object
+#' @return the name of the data as a string
+#' @noRd
+#'
+get_data_name <- function(aov_object) {
+  if (toString(aov_object$call$data) == "") {
+    data_name <- toString(aov_object$call$formula[[2]][[2]])
+  } else {
+    data_name <- toString(aov_object$call$data)
+  }
+  return(data_name)
+}
+
+#' Write JSON-LD from an ANOVA object
+#' @param aov_object
+#' @return JSON-LD string
+#' @noRd
+#'
+write_stats_aov <- function(aov_object) {
+  var_names <- names(attributes(aov_object$terms)$dataClasses)
+  target_name <- var_names[1]
+  group_name <- var_names[2]
+  data_name <- get_data_name(aov_object)
+  sum_object <- summary(aov_object)[[1]]
+  dt <-
+    dtreg::load_datatype("https://doi.org/21.T11969/b9335ce2c99ed87735a6")
+  soft_method <- add_soft_method(dt, "stats;aov")
+  soft_method$is_implemented_by <- deparse(aov_object$call)
+  dim_input <-
+    dt$matrix_size(
+      number_of_rows = nrow(aov_object$model),
+      number_of_columns = ncol(aov_object$model)
+    )
+  input <- dt$data_item(label = data_name,
+                        has_characteristic = dim_input)
+  target_variable <-
+    dt$component(label = target_name)
+  output <- dt$data_item(label = "ANOVA results",
+                         source_table = data.frame(sum_object))
+  instance <- dt$group_comparison(
+    label = paste0("Anova ", target_name, " vs ", group_name),
+    executes = soft_method,
+    has_input = input,
+    targets = target_variable,
+    has_output = output
+  )
+  json_object <- dtreg::to_jsonld(instance)
+  return(json_object)
+}
+
+#' Wrap stats::aov function
+#' @param ... the same arguments as in the wrapped function
+#' @return a list of ANOVA object and JSN-LD
+#' @export
+#'
+#' @examples
+#' results <- stats_aov(Petal.Length ~ Species, data = iris)
+#'
+stats_aov <- function(...) {
+  call <- match.call()
+  aov_object <- stats::aov(...)
+  aov_object$call <- call
+  json_object <- write_stats_aov(aov_object)
+  result <- list(aov_object, json_object)
+  names(result) <- c("anova", "json_ld")
+  return(result)
+}
