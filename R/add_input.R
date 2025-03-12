c9a638a5351607a50dfaabcb78218fce4e17774c
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2025-03-12 12:47:32 +0100

    check type of input

--- R/add_input.R
+++ R/add_input.R
@@ -14,16 +14,22 @@ add_input <- function(dt, input_data) {
       )
     inputs <- dt$data_item(has_characteristic = dimensions_input)
   } else if (is.list(input_data)) {
-    inputs <- list()
-    for (name in names(input_data)) {
-      dimensions_inp <- dt$matrix_size(
-        number_of_rows = nrow(input_data[[name]]),
-        number_of_columns = ncol(input_data[[name]])
-      )
-      inp_inst <- dt$data_item(label = name,
-                               has_characteristic = dimensions_inp)
-      inputs <- append(inputs, inp_inst)
+    if (is.null(names(input_data))) {
+      stop("You input_data list should be named. See 'named list in R'.")
+    } else{
+      inputs <- list()
+      for (name in names(input_data)) {
+        dimensions_inp <- dt$matrix_size(
+          number_of_rows = nrow(input_data[[name]]),
+          number_of_columns = ncol(input_data[[name]])
+        )
+        inp_inst <- dt$data_item(label = name,
+                                 has_characteristic = dimensions_inp)
+        inputs <- append(inputs, inp_inst)
+      }
     }
+  } else {
+    stop("Argument data_input should be either a dataframe or a list")
   }
   return(inputs)
 }
