18930d697d6cd24d87ac721a91f4c472f9bd0418
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2025-05-09 16:20:20 +0200

    add level

---
+++ R/add_level.R
@@ -0,0 +1,25 @@
+#' Title
+#'
+#' @param dt A datatype loaded with the dtreg package
+#' @param code_string a line of code as a string
+#' @return an instance of input class
+#' @noRd
+#'
+add_level <- function(dt, code_string) {
+  parts <- parse_code_string(code_string)
+  level_name <- parts$level_name
+  if (is.list(level_name)) {
+    level <- list()
+    for (name in level_name) {
+      level_inst <- dt$component(label = name)
+      level <- append(level, level_inst)
+    }
+  } else {
+    if (is.na(level_name)) {
+      warning("Level label is not available, you can set it manually",
+              call. = FALSE)
+    }
+    level <- dt$component(label = level_name)
+  }
+  return(level)
+}
