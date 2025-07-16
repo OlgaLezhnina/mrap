62dce00ed3840def9f8b5d5c0cbe14e6ef5c1cb2
Olga Lezhnina <Olga.Lezhnina@tib.eu>
2025-07-16 16:52:35 +0200

    document to_jsonld

--- R/to_jsonld.R
+++ R/to_jsonld.R
@@ -1,2 +1,8 @@
-#' Wrapper for dtreg function to_jsonld
+#' Write an instance in JSON-LD format
+#' @description
+#' This function is imported from dtreg for ease-of-use
+#' @param instance An instance of an R6 class
+#' @return JSON string in JSON-LD format
+#' @export
+#' @examples
 to_jsonld <- dtreg::to_jsonld
